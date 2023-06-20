806c8d156a6685b0f363f0c038059600c45e1da4
AlexCh1975 <stovertikal@mail.ru>
2023-06-21 02:26:15 +0700

    изменения: блок offer, появление menu__hidden, добавлена анимация, изменена анимация social__link, DZ_16

--- app/script.js
+++ app/script.js
@@ -1,13 +1,21 @@
 
 const menu = document.getElementById('nav__menu');
 
-document.addEventListener('click', function(e){
-    if (e.target.id != 'menu__btn' && e.target.id != 'nav__menu'){
-        // menu.style.display = 'none';
-        menu.style.visibility = 'hidden';
-    }else if(e.target.id == 'menu__btn'){
-        // menu.style.display = (menu.style.display != 'block') ? 'block' : 'none';
-        menu.style.visibility = (menu.style.visibility != 'visible') ? 'visible' : 'hidden';
-    }
+// document.addEventListener('click', function(e){
+//     if (e.target.id != 'menu__btn' && e.target.id != 'nav__menu'){
+//         // menu.style.display = 'none';
+//         menu.style.visibility = 'hidden';
+//     }else if(e.target.id == 'menu__btn'){
+//         // menu.style.display = (menu.style.display != 'block') ? 'block' : 'none';
+//         menu.style.visibility = (menu.style.visibility != 'visible') ? 'visible' : 'hidden';
+//     }
     
-});
+// });
+
+document.addEventListener("click", function(e) {
+    if (e.target.id != 'menu__btn' && e.target.id != 'nav__menu') {
+      menu.style.transform = 'translateX(150%)';
+    } else if (e.target.id == 'menu__btn') {
+      menu.style.transform = (menu.style.transform != 'translateX(0)') ? 'translateX(0)' : 'translateX(150%)';
+    }
+  });
